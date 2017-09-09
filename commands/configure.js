82ef4184ec833a9171de67ca12471886df7f571d
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-09-09 17:05:20 +0200

    mods

---
+++ commands/configure.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  description: 'Configure you aws credentials',
+  fn: ({ l, commands, args }) => {
+
+  }
+};
--- commands/create.js
+++ commands/create.js
@@ -1,8 +1,24 @@
 'use strict';
-
+const inquirer = require('inquirer');
+const AWS = require('aws-sdk');
 module.exports = {
   description: 'Create a new Valkyrie application',
   fn: ({ l, commands, args }) => {
-
+    const notNullValidator = (val) => val !== '';
+    inquirer.prompt([
+      { type: 'input', name: 'name', message: 'Project name:', validate: notNullValidator },
+      { type: 'input', name: 'region', message: 'Region name:', validate: notNullValidator }
+    ])
+      .then(results => {
+        const apigateway = new AWS.APIGateway({ region: results.region });
+        apigateway.createRestApi({
+          name: results.name,
+          description: 'Valkyrie application'
+        }, (err, data) => {
+          if (err) return l.error(err);
+          l.success(data);
+        });
+      })
+      .catch(l.error);
   }
 };
