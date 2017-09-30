970cec409509acd0713ff6f680e30bbef900d407
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-09-30 21:11:38 +0200

    wrk in progress

--- commands/update.js
+++ commands/update.js
@@ -1,6 +1,7 @@
 'use strict';
 const { promisify } = require('util');
 const zipdir = promisify(require('zip-dir'));
+const inquirer = require('inquirer');
 const { getProjectInfo, getAWSCredentials } = require('../utils');
 
 const AWS = require('aws-sdk');
@@ -21,8 +22,9 @@ module.exports = {
     const { valkconfig, root } = getProjectInfo();
     const promises = [];
     const lambda = new AWS.Lambda(Object.assign({ region: valkconfig.Project.Region }, { credentials: getAWSCredentials() }));
+    /*
     if ((!argv.code && !argv.config) || argv.code) promises.push(new Promise((resolve, reject) => {
-      l.log('updating lambda code...');
+      l.log(`updating ${l.colors[env === 'staging' ? 'cyan' : 'magenta']}${env}${l.colors.reset} lambda code...`);
       zipdir(root)
         .then(ZipFile => lambda.updateFunctionCode({ FunctionName: valkconfig.Lambda.FunctionName, ZipFile }).promise())
         .then(() => l.success('lambda code updated;'))
@@ -37,8 +39,24 @@ module.exports = {
         .then(resolve)
         .catch(reject);
     }));
-
-    Promise.all(promises)
+*/
+    Promise.resolve()
+      .then(() => {
+        if (Object.keys(valkconfig.Environments).length > 1) {
+          return inquirer.prompt([
+            { type: 'list', name: 'env', message: 'select which environment you want to update:', choices: ['Staging', 'Production'], default: 0 }
+          ]);
+        }
+      })
+      .then(() => {
+        if ((!argv.code && !argv.config)) {
+          return inquirer.prompt([
+            { type: 'checkbox', name: 'update', message: 'what do you want to update?:', choices: [{ name: 'code', checked: true }, { name: 'config', checked: false }], validate: (choices) => choices.length ? true : 'select at least one;' }
+          ]);
+        } else return { update: ['code', 'config'].filter(e => argv[e]) };
+      })
+      .then(console.log)
+      .then(() => Promise.all(promises))
       .then(resolve)
       .catch(reject);
   })
