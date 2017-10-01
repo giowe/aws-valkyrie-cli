c82afc8a0aa70a68fd03a4a614661ea1c63379a0
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-10-01 17:25:27 +0200

    typo in help

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,13 @@
 module.exports = {
   description: 'Shows all commands info',
   fn: ({ l, commands }) => new Promise((resolve) => {
-    l.log('Help:', [
+    l.log('Help:');
+    l.log([
       ...Object.entries(commands).map(([command, { flags = [], description }]) => [
         `${l.colors.white}${l.leftPad(command, 15)}${l.colors.reset}${description}`,
         ...flags.map(({ name, short, description }) => `${l.leftPad('', 17)}${l.leftPad(`--${name}${short? ` -${short}`:''}`, 16)} ${description}`)
       ].join('\n'))
-    ].join('\n\n'));
+    ].join('\n\n'), { prefix: false });
     resolve();
   })
 };
