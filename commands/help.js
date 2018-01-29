893bd38f08c774ce4aec87b9abece7d97b7358cc
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-01-29 14:52:15 +0100

    mods

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,8 @@ module.exports = {
     l.log('Help:');
     l.log([
       ...Object.entries(commands).filter(([, {hidden}]) => !hidden).map(([command, {flags = [], description}]) => [
-        `${l.colors.white}${l.leftPad(command, 15)}${l.colors.reset}${description}`,
-        ...flags.map(({name, short, description}) => `${l.leftPad('', 17)}${l.leftPad(`--${name}${short? ` -${short}`:''}`, 16)} ${description}`)
+        `${l.colors.yellow} ${`${command}: `.padStart(10)} ${l.colors.reset}${description}`,
+        ...flags.map(({name, short, description}) => `${''.padStart(17)}${`--${name}${short? ` -${short}`:''}`} ${description}`)
       ].join('\n'))
     ].join('\n\n'), {prefix: false});
     resolve();
