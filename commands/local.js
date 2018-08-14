c1d3bef5191bbaa7391fb471bd76a94875d8933c
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-08-14 16:08:20 +0200

    updated dependencies

--- commands/local.js
+++ commands/local.js
@@ -1,40 +1,40 @@
-const {logger: l} = require('aws-valkyrie-utils');
-const proxyLocal = require('aws-apigateway-proxy-local');
-const path = require('path');
-const argv = require('simple-argv');
-const {getProjectInfo} = require('../utils');
+const { logger: l } = require("aws-valkyrie-utils")
+const proxyLocal = require("aws-apigateway-proxy-local")
+const path = require("path")
+const argv = require("simple-argv")
+const { getProjectInfo } = require("../utils")
 
 // TODO, to review
-let valkconfig;
+let valkconfig
 try {
-  valkconfig = getProjectInfo().valkconfig;
+  valkconfig = getProjectInfo().valkconfig
 } catch(e) {}
 
 module.exports = {
-  description: 'Runs locally your Valkyrie application;',
+  description: "Runs locally your Valkyrie application;",
   flags: [
     {
-      name: 'env',
-      short: 'e',
-      description: `Set the environment${valkconfig ? ` (default to ${valkconfig.LocalEnv})` : ''};`
+      name: "env",
+      short: "e",
+      description: `Set the environment${valkconfig ? ` (default to ${valkconfig.LocalEnv})` : ""};`
     },
     {
-      name: 'port',
-      short: 'p',
-      description: 'Set the local port, default to 8000;'
+      name: "port",
+      short: "p",
+      description: "Set the local port, default to 8000;"
     },
     {
-      name: 'profile',
-      description: 'Uses a specific profile instead of the default one;'
+      name: "profile",
+      description: "Uses a specific profile instead of the default one;"
     }
   ],
   fn: () => new Promise(() => {
-    const {root, valkconfig} = getProjectInfo();
+    const { root, valkconfig } = getProjectInfo()
     if (!valkconfig.LocalEnv) {
-      throw new Error('missing LocalEnv key in valkconfig.json');
+      throw new Error("missing LocalEnv key in valkconfig.json")
     }
-    const [fileName, handler] = valkconfig.Environments[valkconfig.LocalEnv].Lambda.Handler.split('.');
-    const lambdaFn = require(path.join(root, fileName));
-    proxyLocal(argv.port || argv.p || 8000, lambdaFn, handler, {}, {log: l.log, error: l.error, success: l.success});
+    const [fileName, handler] = valkconfig.Environments[valkconfig.LocalEnv].Lambda.Handler.split(".")
+    const lambdaFn = require(path.join(root, fileName))
+    proxyLocal(argv.port || argv.p || 8000, lambdaFn, handler, {}, { log: l.log, error: l.error, success: l.success })
   })
-};
+}
--- logger.js
+++ logger.js
@@ -1,114 +1,114 @@
 /* eslint-disable no-console */
-const argv = require('simple-argv');
-const {Spinner} = require('cli-spinner');
+const argv = require("simple-argv")
+const { Spinner } = require("cli-spinner")
 
 const colors = {
-  'reset': '\x1b[0m',
-  'bright': '\x1b[1m',
-  'dim': '\x1b[2m',
-  'underscore': '\x1b[4m',
-  'blink': '\x1b[5m',
-  'reverse': '\x1b[7m',
-  'hidden': '\x1b[8m',
-  'black': '\x1b[30m',
-  'red': '\x1b[31m',
-  'green': '\x1b[32m',
-  'yellow': '\x1b[33m',
-  'blue': '\x1b[34m',
-  'magenta': '\x1b[35m',
-  'cyan': '\x1b[36m',
-  'white': '\x1b[37m',
-  'crimson': '\x1b[38m',
-  'bg': {
-    'black': '\x1b[40m',
-    'red': '\x1b[41m',
-    'green': '\x1b[42m',
-    'yellow': '\x1b[43m',
-    'blue': '\x1b[44m',
-    'magenta': '\x1b[45m',
-    'cyan': '\x1b[46m',
-    'white': '\x1b[47m',
-    'crimson': '\x1b[48m'
+  "reset": "\x1b[0m",
+  "bright": "\x1b[1m",
+  "dim": "\x1b[2m",
+  "underscore": "\x1b[4m",
+  "blink": "\x1b[5m",
+  "reverse": "\x1b[7m",
+  "hidden": "\x1b[8m",
+  "black": "\x1b[30m",
+  "red": "\x1b[31m",
+  "green": "\x1b[32m",
+  "yellow": "\x1b[33m",
+  "blue": "\x1b[34m",
+  "magenta": "\x1b[35m",
+  "cyan": "\x1b[36m",
+  "white": "\x1b[37m",
+  "crimson": "\x1b[38m",
+  "bg": {
+    "black": "\x1b[40m",
+    "red": "\x1b[41m",
+    "green": "\x1b[42m",
+    "yellow": "\x1b[43m",
+    "blue": "\x1b[44m",
+    "magenta": "\x1b[45m",
+    "cyan": "\x1b[46m",
+    "white": "\x1b[47m",
+    "crimson": "\x1b[48m"
   }
-};
-const prefix = `[${colors.yellow}VALK${colors.reset}]`;
+}
+const prefix = `[${colors.yellow}VALK${colors.reset}]`
 
-function leftPad(text, len, char = ' ', alignment = 'left') {
-  text = text.toString();
-  const l = len - text.length;
+function leftPad(text, len, char = " ", alignment = "left") {
+  text = text.toString()
+  const l = len - text.length
   for (let i = 0; i < l; i++) {
-    if (alignment === 'left') text += char;
-    else text = char + text;
+    if (alignment === "left") text += char
+    else text = char + text
   }
-  return text;
+  return text
 }
 
 function repeat(text, len) {
-  let out = '';
-  for (let i = 0; i < len; i++) out += text;
-  return out;
+  let out = ""
+  for (let i = 0; i < len; i++) out += text
+  return out
 }
 
 function log(color, ...args) {
-  stopSpinner();
-  const options = {prefix: true, inline: false};
-  if (typeof args[args.length -1] === 'object') Object.assign(options, args.pop());
-  args.unshift(color);
-  if (options.prefix) args.unshift(prefix);
+  stopSpinner()
+  const options = { prefix: true, inline: false }
+  if (typeof args[args.length - 1] === "object") Object.assign(options, args.pop())
+  args.unshift(color)
+  if (options.prefix) args.unshift(prefix)
 
-  if (!options.inline) console.log(...args, colors.reset);
+  if (!options.inline) console.log(...args, colors.reset)
   else process.stdout.write(`${args.map(arg => {
     switch (typeof arg) {
-      case 'string': return arg;
-      case 'object': return JSON.stringify(arg, null, 2);
-      default: return '' + arg;
+      case "string": return arg
+      case "object": return JSON.stringify(arg, null, 2)
+      default: return "" + arg
     }
-  }).join(' ')}${colors.reset}`);
+  }).join(" ")}${colors.reset}`)
 }
 
-function frame(text, options = {prefix: true}) {
-  stopSpinner();
-  const border = repeat('─', text.replace(/\u001b\[.*?m/g, '').length + 2);
-  const padding = options.prefix ? repeat(' ', 7) : '';
+function frame(text, options = { prefix: true }) {
+  stopSpinner()
+  const border = repeat("─", text.replace(/\u001b\[.*?m/g, "").length + 2)
+  const padding = options.prefix ? repeat(" ", 7) : ""
   console.log([
     `${padding}┌${border}┐`,
-    `${options.prefix ? `${prefix} ` : '' }│ ${text} │`,
+    `${options.prefix ? `${prefix} ` : "" }│ ${text} │`,
     `${padding}└${border}┘`
-  ].join('\n'));
+  ].join("\n"))
 }
 
 function fail(...args) {
-  log(`[${colors.red}FAILURE${colors.reset}]`, ...args);
+  log(`[${colors.red}FAILURE${colors.reset}]`, ...args)
 }
 
 function error(err) {
-  log(`[${colors.red}ERROR${colors.reset}]`, argv.debug ? `\n${err.stack}` : err.message);
+  log(`[${colors.red}ERROR${colors.reset}]`, argv.debug ? `\n${err.stack}` : err.message)
 }
 
 function warning(err) {
-  log(`[${colors.yellow}WARNING${colors.reset}]`, argv.debug ? `\n${err.stack}` : err.message);
+  log(`[${colors.yellow}WARNING${colors.reset}]`, argv.debug ? `\n${err.stack}` : err.message)
 }
 
 function success(...args) {
-  log(`[${colors.green}SUCCESS${colors.reset}]`, ...args);
+  log(`[${colors.green}SUCCESS${colors.reset}]`, ...args)
 }
 
 function debug(...args) {
-  if (argv.debug) log(`[${colors.magenta}DEBUG${colors.reset}]`, ...args);
+  if (argv.debug) log(`[${colors.magenta}DEBUG${colors.reset}]`, ...args)
 }
 
-const spinner = new Spinner();
-spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏');
+const spinner = new Spinner()
+spinner.setSpinnerString("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏")
 function wait(...args) {
-  stopSpinner();
-  const options = {prefix: true};
-  if (typeof args[args.length -1] === 'object') Object.assign(options, args.pop());
-  spinner.text = `${options.prefix ? `${prefix} `: ''}[${colors.white}WAIT${colors.reset}] ${args.join(' ')} %s`;
-  spinner.start();
+  stopSpinner()
+  const options = { prefix: true }
+  if (typeof args[args.length - 1] === "object") Object.assign(options, args.pop())
+  spinner.text = `${options.prefix ? `${prefix} ` : ""}[${colors.white}WAIT${colors.reset}] ${args.join(" ")} %s`
+  spinner.start()
 }
 
 function stopSpinner() {
-  if (spinner.isSpinning()) spinner.stop(true);
+  if (spinner.isSpinning()) spinner.stop(true)
 }
 
 module.exports = {
@@ -122,4 +122,4 @@ module.exports = {
   debug,
   wait,
   colors
-};
+}
