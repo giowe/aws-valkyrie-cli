473e340bd38f50c07fbb319b00aa1a3a80060028
Nicolò Fuccella <nico.fuccella@gmail.com>
2018-08-22 18:19:58 +0200

    Assume role
    Now when you use valk local, the cli tries to assume the role
    Fix https://github.com/giowe/aws-valkyrie-cli/issues/3

--- commands/local.js
+++ commands/local.js
@@ -2,7 +2,8 @@ const l = require("../logger.js")
 const proxyLocal = require("aws-apigateway-proxy-local")
 const { join } = require("path")
 const argv = require("simple-argv")
-const { getProjectInfo } = require("../utils")
+const { getProjectInfo, getRequiredEnv, getDefaultProfile, getAWSCredentials } = require("../utils")
+const { role: { assumeRole } } = require("../lib/iam.js")
 
 // TODO, to review
 let valkconfig
@@ -31,19 +32,47 @@ module.exports = {
   ],
   fn: () => new Promise(() => {
     const { root, valkconfig } = getProjectInfo()
-    if (!valkconfig.LocalEnv) {
-      throw new Error("missing LocalEnv key in valkconfig.json")
-    }
-    const [fileName, handler] = valkconfig.Environments[valkconfig.LocalEnv].Lambda.Handler.split(".")
+    // if (!valkconfig.LocalEnv) {
+    //   throw new Error("missing LocalEnv key in valkconfig.json")
+    // }
+    const state = {}
+    return Promise.resolve()
+      .then(() => {
+        let selectedEnv
+        if (Object.keys(valkconfig.Environments).some(env => {
+          if (argv[env]) {
+            selectedEnv = env
+            return true
+          } else {
+            return false
+          }
+        })) {
+          return selectedEnv
+        } else {
+          return getRequiredEnv(valkconfig).then(({ env }) => env)
+        }
+      })
+      .then(env => {
+        const { Lambda: { Role } } = valkconfig.Environments[env]
+        state.env = env
+        state.role = Role
+        return assumeRole(Role, getAWSCredentials(argv.profile || getDefaultProfile()), valkconfig.Project.Region)
+      })
+      .then(() => {
+        const { Lambda: { Environment: { Variables: envVariables }, Handler } } = valkconfig.Environments[state.env]
 
-    // this enables lambda env variables during local execution
-    Object.entries(valkconfig.Environments[valkconfig.LocalEnv].Lambda.Environment.Variables).forEach(([key, value]) => process.env[key] = value)
+        const [fileName, handler] = Handler.split(".")
 
-    const port = argv.port || argv.p || 8000
-    proxyLocal(require(join(root, fileName))[handler], {
-      port,
-      logger: { log: l.log, error: l.error, success: l.success },
-      listeningMessage: `Valkyrie local listening on port ${port}`
-    })
+        // this enables lambda env variables during local execution
+        Object.entries(envVariables).forEach(([key, value]) => process.env[key] = value)
+
+        const port = argv.port || argv.p || 8000
+        proxyLocal(require(join(root, fileName))[handler], {
+          port,
+          logger: { log: l.log, error: l.error, success: l.success },
+          listeningMessage: `Valkyrie local listening on port ${port}`
+        })
+      })
+      .catch(l.error)
   })
 }
